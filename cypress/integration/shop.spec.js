3ee0013d5d339219b4d9e69e5e893dba036f67ea
VitorMarinheiro <vitor.marinheiro16@gmail.com>
2020-10-16 17:51:34 -0300

    Adicao do fluxo de compra.

--- cypress/integration/shop.spec.js
+++ cypress/integration/shop.spec.js
@@ -71,4 +71,71 @@ describe('MyStore', () => {
         cy.get('.page-heading').should('be.visible').should('have.text', 'My account')  // valida se elemento esta visivel e contem o texto 'My account'
 
     });
+
+    it('Realizar uma compra', () =>{
+
+        // Busca por 'Dress' no campo de busca
+        cy.get('[id=search_query_top').type('dress')
+        cy.get('#searchbox > .btn').click()
+
+        // Ordena produtos por maior preço e adiciona ao carrinho o primeiro vestido
+        cy.get('#selectProductSort').select('Price: Highest first')
+        
+        // Captura o valor unitario do produto para posterior validacao desse valor na chamada da API de adicionar ao carrinho
+        cy.get(':nth-child(1) > .product-container > .right-block > .content_price > .price').invoke('text').then(($preco) => {
+          
+            // Cria um route para validar a API chamada
+            cy.server()
+            cy.route('POST', '/index.*').as('newDress')
+
+            // Insere ao carrinho
+            cy.get(':nth-child(1) > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
+
+            // Captura as informacoes da API no XHR e valida dados
+            cy.wait('@newDress').then((xhr => {
+                expect(xhr.status).be.eq(200)
+                expect(xhr.response.body).has.property('hasError', false)
+                expect(xhr.response.body).has.property('nbTotalProducts', 1)
+                expect(xhr.response.body).has.property('productTotal', $preco.trim())
+            }))
+
+        })
+
+        // Incrementa 1 unidade no produto 
+        cy.get('.button-container > .button-medium > span').click()
+        cy.get('#cart_quantity_up_4_16_0_0 > span').click()
+        cy.get('.cart_quantity_input').should('have.value', 2)
+
+        // Valida se o valor multiplicado por 2x corresponde ao total
+        cy.get('#product_price_4_16_0 > .price').invoke('text').then(($valor) => {
+            cy.get('[id=total_product_price_4_16_0]').invoke('text').should(($total) => {
+              expect(parseFloat($valor.replace('$', ''))*2).eq(parseFloat($total.replace('$', '')))
+            })
+        })
+
+        // Finaliza a selecao do produto
+        cy.get('.cart_navigation > .button > span').click()
+
+        // Insere as informacoes salvas nas variaveis do Cypress para o login
+        cy.get('[id=email]').type(Cypress.env('userEmail'))
+        cy.get('[id=passwd]').type(Cypress.env('userPwd'))
+        cy.get('#SubmitLogin > span').click()
+
+        // Finaliza o endereco
+        cy.get('#ordermsg > .form-control').type('Comprando dois vestidos.')
+        cy.get('.cart_navigation > .button > span').click()
+
+        // Altera o Shipping
+        cy.get('#cgv').check()
+        cy.get('.cart_navigation > .button > span').click()
+
+        // Seleciona metodo de pagamento
+        cy.get('.bankwire').click()
+        cy.get('#cart_navigation > .button > span').click()
+
+        // Validar ordem finalizada
+        cy.get('.cheque-indent > .dark').should('be.visible')
+
+    })
+
 })
